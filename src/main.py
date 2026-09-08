b3f7adb237bbb41cee1e706774626c0e335d8450
brendannorris-dev <norrisbrendanm@gmail.com>
2026-09-08 03:03:10 -0500

    Add src, main.py, textnode.py, and main.sh

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main():
+    test = TextNode("This is some anchor text", TextType.LINK_TEXT, "https://www.boot.dev")
+    print(test.__repr__())
+
+main()
