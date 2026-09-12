d359cf223765a2dc0d32da5905c3ae1bf5436f7f
brendannorris-dev <norrisbrendanm@gmail.com>
2026-09-11 21:13:05 -0500

    Add markdown converter and unit testing

---
+++ src/markdown_converter.py
@@ -0,0 +1,26 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        new_text = node.text.split(delimiter)
+
+        if len(new_text) % 2 == 0:
+            raise ValueError(f"Incorrect markdown formatting: {delimiter} section not closed")
+
+        split_nodes = []
+
+        for i in range(len(new_text)):
+            if new_text[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(new_text[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(new_text[i], text_type))
+        new_nodes.extend(split_nodes)
+            
+    return new_nodes
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
 from textnode import TextNode, TextType, text_node_to_html_node
+from markdown_converter import split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -56,6 +57,132 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.value,"")
         self.assertEqual(html_node.props, {"src":"https://example.com/cat.png", "alt": "This is an image node"})
 
+    def test_split_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes,
+                        [
+                            TextNode("This is text with a ", TextType.TEXT),
+                            TextNode("code block", TextType.CODE),
+                            TextNode(" word", TextType.TEXT)
+                        ] )
 
+    def test_split_bold(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes,
+                    [
+                        TextNode("This is text with a ", TextType.TEXT),
+                        TextNode("bold", TextType.BOLD),
+                        TextNode(" word", TextType.TEXT)
+                    ] )
+        
+    def test_split_italic(self):
+        node = TextNode("This is text with an _italic_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_nodes,
+                    [
+                        TextNode("This is text with an ", TextType.TEXT),
+                        TextNode("italic", TextType.ITALIC),
+                        TextNode(" word", TextType.TEXT)
+                    ] )
+
+    def test_nontext_node(self):
+        bold_node = TextNode("already bold", TextType.BOLD)
+        text = TextNode("has `code` here", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([bold_node, text], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes),4)
+        self.assertEqual(new_nodes,
+                         [
+                             TextNode("already bold", TextType.BOLD),
+                             TextNode("has ", TextType.TEXT),
+                             TextNode("code", TextType.CODE),
+                             TextNode(" here", TextType.TEXT)
+                         ])
+
+    def test_multiple_sections(self):
+        node = TextNode("Bold **words** are **great**, right?", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(new_nodes), 5)
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes,
+                         [
+                             TextNode("Bold ", TextType.TEXT),
+                             TextNode("words", TextType.BOLD),
+                             TextNode(" are ", TextType.TEXT),
+                             TextNode("great", TextType.BOLD),
+                             TextNode(", right?", TextType.TEXT)
+                         ])
+
+    def test_delimiter_at_start(self):
+        node = TextNode("**Bold** words are great", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(new_nodes),2)
+        self.assertEqual(new_nodes,
+                         [
+                             TextNode("Bold", TextType.BOLD),
+                             TextNode(" words are great", TextType.TEXT)
+                         ])
+        
+    def test_delimiter_at_end(self):
+        node = TextNode("Bold words are **great**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(new_nodes),2)
+        self.assertEqual(new_nodes,
+                          [
+                              TextNode("Bold words are ", TextType.TEXT),
+                              TextNode("great", TextType.BOLD)
+                          ])
+
+    def test_whole_delimited(self):
+        node = TextNode("**Bold is amazing**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(new_nodes),1)
+        self.assertEqual(new_nodes,
+                         [
+                             TextNode("Bold is amazing", TextType.BOLD)
+                         ])
+
+    def test_unclosed_delimiter(self):
+        node = TextNode("Oopsies, forgot to **end bold", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+
+    def test_no_delimiter(self):
+        node = TextNode("Oops, no markdown!", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertEqual(new_nodes,
+                         [
+                             TextNode("Oops, no markdown!", TextType.TEXT)])
+
+    def test_empty_list(self):
+        new_nodes = split_nodes_delimiter([], '**', TextType.BOLD)
+        self.assertEqual(new_nodes, [])
+
+    def test_multiple_text_nodes(self):
+        node1 = TextNode("This is **bold** node1", TextType.TEXT)
+        node2 = TextNode("This is **bold** node2", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node1,node2], '**', TextType.BOLD)
+        self.assertEqual(new_nodes,
+                         [
+                             TextNode("This is ", TextType.TEXT),
+                             TextNode("bold", TextType.BOLD),
+                             TextNode(" node1", TextType.TEXT),
+                             TextNode("This is ", TextType.TEXT),
+                             TextNode("bold", TextType.BOLD),
+                             TextNode(" node2", TextType.TEXT)
+                         ])
+
+    def test_all_nontext_nodes(self):
+        node1 = TextNode("This is bold1", TextType.BOLD)
+        node2 = TextNode("This is code1", TextType.CODE)
+        new_nodes = split_nodes_delimiter([node1, node2], '**', TextType.BOLD)
+        self.assertEqual(new_nodes,
+                         [
+                             TextNode("This is bold1", TextType.BOLD),
+                             TextNode("This is code1", TextType.CODE)
+                         ])
+        
 if __name__ == "__main__":
     unittest.main()
